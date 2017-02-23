61ae8555d7f9f73b5be3cdb842b21af81ca22477
dq451284258 <451284258@qq.com>
2017-02-23 13:48:23 +0800

    Update MyLittle.js

--- MyLittle.js
+++ MyLittle.js
@@ -25,3 +25,35 @@ function addUrlPara(key,val){
 	}
 	window.location.href = cururl.split('?')[0] + '?' + newurl;
 }
+
+//创建select
+var MessageInit = function(message,defalut,List)//message('id');default(默认值);List(列表,数组);
+{
+	var cmbProvince = document.getElementById(message);
+	function cmbAddOption(cmb, str, obj)
+	{
+		var option = document.createElement("OPTION");
+		cmb.options.add(option);
+		option.innerText = str;
+		option.value = str;
+		option.obj = obj;
+	}
+	console.log(worklist[List]);
+	List = worklist[List];
+	for(var i=0; i<List.length; i++)
+	{
+		cmbAddOption(cmbProvince, List[i].name, List[i]);
+	}
+	cmbSelect(cmbProvince,defalut);
+	function cmbSelect(cmb, str)
+	{
+		for(var i=0; i<cmb.options.length; i++)
+		{
+			if(cmb.options[i].value == str)
+			{
+				cmb.selectedIndex = i;
+				return;
+			}
+		}
+	}
+};
